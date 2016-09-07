5b2e7bd94866d89034a999ae989279b5ecdd70c8
Sebastien Chatelle Blanchard <sebastien.chatelle.blanchard@gmail.com>
2016-09-06 22:50:06 -0400

    06-09-2016 update

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+def ftoc(f)
+	(f -32) * 5/9
+end
+
+def ctof(c)
+	c * 1.8 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add (x, y)
+	x + y
+end
+
+def subtract (x, y)
+	x - y
+end
+
+def sum(array)
+	array.inject(0, :+)
+end
+
+def multiply(array)
+	array.inject(:*)
+end
+
+def power(x, y)
+	x**y
+end 
+
+def factorial(n)
+	if n == 0
+		return 1
+	else
+		return n*factorial(n-1)
+	end
+end
